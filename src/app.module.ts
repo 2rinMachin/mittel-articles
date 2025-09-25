4d32e8408c4540278b5d4ec277586385f9a910c2
José Daniel Grayson <josedanielgrayson@proton.me>
2025-09-25 12:42:38 -0500

    fix: remove hardcoded mongo uri and add @nestjs/config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,12 @@ import { ArticlesModule } from './articles/articles.module';
 import { CommentsModule } from './comments/comments.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { EventEmitterModule } from '@nestjs/event-emitter';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/mittel-articles'),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI ?? ''),
     EventEmitterModule.forRoot({ global: true }),
     ArticlesModule,
     CommentsModule,
